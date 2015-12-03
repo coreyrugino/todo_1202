02af91c564b241d58bb0625f30aa81a018c79e07
Corey Rugino <coreyrugino@gmail.com>
2015-12-02 17:51:07 -0700

    working home and about static pages, edit items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,14 +20,25 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-    private
 
-    def item_params
-      params.require(:item).permit(:title, :detail)
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
     end
+  end
 
+  private
 
-
-
+  def item_params
+    params.require(:item).permit(:title, :detail)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about', as: 'about'
+
 resources :items
 
-  resources :todos
+  #resources :todos
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
